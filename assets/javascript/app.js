91e4ec95de97de289d7a8113336d44a5e0159efc
Ali Rahimlou <Rahimlou91@gmail.com>
2019-09-20 01:38:32 -0400

    questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+    console.log("ready!");
+
+    var options = [
+        {
+            question: "What is the capital city of Spain?", 
+            choice: ["Guatemala", "El Salvadore", "Peru", "Madrid"],
+            answer: 3,
+         },
+         {
+             question: "What popular soda beverage was originally developed as a mixer for whiskey?", 
+            choice: ["Mountain Dew", "Sprite", "7-UP", "Coke"],
+            answer: 0,
+         }, 
+         {
+             question: "What is the world's longest river?", 
+            choice: ["Mississippi", "Nile", "Amazon", "Long" ],
+            answer: 2,
+        }, 
+        {
+            question: "Name the only heavyweight boxing champion to finish his career of 49 fights without ever having been defeated?", 
+            choice: ["Floyd Maywheather", "Mike Tyson", "Rocky Marciano", "Muhammed Ali" ],
+            answer: 2,
+        }, 
+        {
+            question: "What is the diameter of Earth?", 
+            choice: [" 6,000 miles", "8,000 miles", "24,000 miles", "13,000 miles" ],
+            answer: 1,
+        }, 
+        {
+            question: "What is the most widely eaten fish in the world?", 
+            choice: ["Tilapia", "Herring", "Sardine", "Tuna" ],
+            answer: 1,
+        }, 
+        {
+            question: "What colour is Absynthe?", 
+            choice: ["White", "Red", "Blue", "Green" ],
+            answer: 3,
+        }, 
+        {
+            question: "Name the seventh planet from the sun.", 
+            choice: ["Uranus", "Pluto", "Earth", "Saturn" ],
+            answer: 0,
+        }];
+
+
+
+
+
+
+
+})
