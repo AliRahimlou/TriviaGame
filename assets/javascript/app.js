46e937fe09173dc35d09a8e086343189c90c089d
Ali Rahimlou <Rahimlou91@gmail.com>
2019-09-22 12:09:39 -0400

    more style

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -33,7 +33,7 @@ $(document).ready(function () {
             answer: 1,
         }, 
         {
-            question: "What colour is Absynthe?", 
+            question: "What color is Absynthe?", 
             choice: ["White", "Red", "Blue", "Green" ],
             answer: 3,
         }, 
@@ -43,6 +43,144 @@ $(document).ready(function () {
             answer: 0,
         }];
 
+
+        var correctCount = 0;
+        var wrongCount = 0;
+        var outOfTime = 0;
+        var timer = 30;
+        var intervalId;
+        var userGuess ="";
+        var running = false;
+        var qCount = options.length;
+        var pick;
+        var questionPicked;
+        var newArray = [];
+        var holder = [];
+        
+
+
+        $("#reset").hide();
+
+$("#start").on("click", function () {
+		$("#start").hide();
+		showQuestion();
+		runTimer();
+		for(var i = 0; i < options.length; i++) {
+    holder.push(options[i]);
+}
+    })
+    
+function runTimer(){
+	if (!running) {
+	intervalId = setInterval(decrement, 1000); 
+	running = true;
+	}
+}
+function decrement() {
+	$("#timer").html("<h2>Time remaining: " + timer + "</h2>");
+	timer --;
+
+	if (timer === 0) {
+		outOfTime++;
+		stopTimer();
+        $("#answerOptions").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+        end()
+	}	
+}
+
+function stopTimer() {
+	running = false;
+	clearInterval(intervalId);
+}
+
+function showQuestion() {
+	questionPicked = Math.floor(Math.random()*options.length);
+	pick = options[questionPicked];
+
+
+		$("#questions").html("<h2>" + pick.question + "</h2>");
+		for(var i = 0; i < pick.choice.length; i++) {
+			var userChoice = $("<button></button>" + "<br>" + "<br>");
+			userChoice.addClass("answerOptions");
+			userChoice.html(pick.choice[i]);
+			userChoice.attr("data-guessvalue", i);
+			$("#answerOptions").append(userChoice);
+
+}
+
+
+$(".answerOptions").on("click", function () {
+	userGuess = parseInt($(this).attr("data-guessvalue"));
+
+	
+	if (userGuess === pick.answer) {
+		stopTimer();
+		correctCount++;
+		userGuess="";
+        $("#answerOptions").html("<h4>Correct!</h4>");
+        end()
+        
+		
+
+	} else {
+		stopTimer();
+		wrongCount++;
+		userGuess="";
+		$("#answerOptions").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+		end()
+    }
+   
+    
+
+})
+}
+
+function end(){
+	newArray.push(pick);
+	options.splice(questionPicked,1);
+
+	setTimeout(function() {
+		$("#answerOptions").empty();
+        timer= 30;
+        
+    if ((wrongCount + correctCount + outOfTime) === qCount) {
+		$("#questions").empty();
+		$("#questions").html("<h3>Game Over!  YOUR STATS: </h3>");
+		$("#answerOptions").append("<h3> Correct: " + correctCount + "</h3>" );
+		$("#answerOptions").append("<h3> Incorrect: " + wrongCount + "</h3>" );
+		$("#answerOptions").append("<h3> Unanswered: " + outOfTime + "</h3>" );
+		$("#reset").show();
+		correctCount = 0;
+		wrongCount = 0;
+		outOfTime = 0;
+
+	} else {
+		runTimer();
+        showQuestion();
+        
+
+    }
+}, 2000);
+
+}
+
+
+
+
+$("#reset").on("click", function() {
+	$("#reset").hide();
+	$("#answerOptions").empty();
+	$("#questions").empty();
+	for(var i = 0; i < holder.length; i++) {
+		options.push(holder[i]);
+	}
+	runTimer();
+    showQuestion();
+
+    
+    
+
+})
 
 
 
